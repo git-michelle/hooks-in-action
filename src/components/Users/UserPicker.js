bf0bb018e751ab8ef2044072045761bdfe14bec5
Michelle <16948138+michellecan@users.noreply.github.com>
2021-09-26 14:43:04 -0700

    created spinner, updated Userpicker to fetch data from db.js

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,16 +1,28 @@
-import React from "react";
-import { users } from "../../static.json";
+import { useState, useEffect } from 'react';
+import Spinner from '../UI/Spinner';
 
 const UserPicker = () => {
-  return (
-    <select>
-      {users.map((user) => (
-        <option key={user.id} value={user.name}>
-          {user.name}
-        </option>
-      ))}
-    </select>
-  );
+	const [users, setUsers] = useState(null);
+
+	useEffect(() => {
+		fetch('http://localhost:3001/users')
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+
+	if (users === null) {
+		return <Spinner />;
+	}
+
+	return (
+		<select>
+			{users.map((user) => (
+				<option key={user.id} value={user.name}>
+					{user.name}
+				</option>
+			))}
+		</select>
+	);
 };
 
 export default UserPicker;
