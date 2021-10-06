36771aec66e056de46fd582201748a6ec1d84117
Michelle <16948138+michellecan@users.noreply.github.com>
2021-10-05 22:16:05 -0700

    updated weekpicker to have a manual text input field

--- src/components/Bookings/WeekPicker.js
+++ src/components/Bookings/WeekPicker.js
@@ -1,32 +1,62 @@
-import { useReducer } from "react";
-import reducer from "./weekReducer";
-import { getWeek } from "../../utils/date-wrangler";
-import { FaChevronLeft, FaChevronRight, FaCalendarDay } from "react-icons/fa";
+import { useReducer, useRef, useState } from 'react';
+import reducer from './weekReducer';
+import { getWeek } from '../../utils/date-wrangler';
+import {
+	FaChevronLeft,
+	FaChevronRight,
+	FaCalendarDay,
+	FaCalendarCheck,
+} from 'react-icons/fa';
 
 const WeekPicker = ({ date }) => {
-  const [week, dispatch] = useReducer(reducer, date, getWeek);
+	const [week, dispatch] = useReducer(reducer, date, getWeek);
 
-  return (
-    <div>
-      <p className="date-picker">
-        <button className="btn" onClick={() => dispatch({ type: "PREV_WEEK" })}>
-          <FaChevronLeft /> <span>Prev</span>
-        </button>
+	const textboxRef = useRef();
+	const [dateText, setDateText] = useState('2021-01-01');
 
-        <button className="btn" onClick={() => dispatch({ type: "TODAY" })}>
-          <FaCalendarDay /> <span>Today</span>
-        </button>
+	const goToDate = () => {
+		dispatch({
+			type: 'SET_DATE',
+			payload: dateText,
+		});
+	};
 
-        <button className="btn" onClick={() => dispatch({ type: "NEXT_WEEK" })}>
-          <FaChevronRight /> <span>Next</span>
-        </button>
-      </p>
+	return (
+		<div>
+			<p className="date-picker">
+				<button className="btn" onClick={() => dispatch({ type: 'PREV_WEEK' })}>
+					<FaChevronLeft /> <span>Prev</span>
+				</button>
 
-      <p>
-        {week.start.toDateString()} - {week.end.toDateString()}
-      </p>
-    </div>
-  );
+				<button className="btn" onClick={() => dispatch({ type: 'TODAY' })}>
+					<FaCalendarDay /> <span>Today</span>
+				</button>
+
+				{/* Textbox  */}
+				<span>
+					<input
+						type="text"
+						ref={textboxRef}
+						placeholder="e.g. 2021-10-05"
+						value={dateText}
+						onChange={(e) => setDateText(e.target.value)}
+					/>
+					<button className="go btn" onClick={goToDate}>
+						<FaCalendarCheck />
+						<span>Go</span>
+					</button>
+				</span>
+
+				<button className="btn" onClick={() => dispatch({ type: 'NEXT_WEEK' })}>
+					<FaChevronRight /> <span>Next</span>
+				</button>
+			</p>
+
+			<p>
+				{week.start.toDateString()} - {week.end.toDateString()}
+			</p>
+		</div>
+	);
 };
 
 export default WeekPicker;
